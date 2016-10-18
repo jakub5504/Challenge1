f3b1551354a4bef6bc8e76a2e1f681e8dd9a4c4c
jbki <jakub.kaminski@gft.com>
2016-10-18 14:01:49 +0200

    Pom changed for web socket enabling

--- src/main/java/com/gft/rxjava/ReactiveJava.java
+++ src/main/java/com/gft/rxjava/ReactiveJava.java
@@ -1,16 +1,17 @@
 package com.gft.rxjava;
 
 
-import com.sun.org.apache.xpath.internal.SourceTree;
+
 import rx.Observable;
-import rx.functions.Action1;
 
 public class ReactiveJava {
 
     public static  void main(String args[]){
-        Observable<String> myObservable = Observable.just("Hello World").subscribe(s -> {
-            System.out.println(s + " -Dan");});
+        Observable.just("Hello World")
+                .map(s -> s + " -Dan ")
+                .map(String::hashCode)
+                .map(i -> Integer.toString(i))
+                .subscribe(System.out::println);
 
-        myObservable.subscribe(onNextAction);
     }
 }
